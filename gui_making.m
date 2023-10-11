ed3e512bbd6c8f0bd28ccf7f085f2617bb625235
david <davidcoelho89@gmail.com>
2023-10-11 10:14:26 -0300

    first samples learned

---
+++ gui_making.m
@@ -0,0 +1,18 @@
+%% GUIDE - MATLAB
+
+% David Nascimento Coelho
+% Última Revisão: 23/05/2014
+
+clear all; close all; clc;
+
+nfig = 0;       % contagem do numero de figuras
+
+%% Função Principal (nova)
+
+appdesigner;
+
+%% Função Principal (antiga)
+
+guide;
+
+%% 
